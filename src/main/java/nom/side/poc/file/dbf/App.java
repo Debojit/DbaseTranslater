c49d0e168ecba1842a0b9e95191177997d8ed46a
Debojit Sinha <sinha.debojit@gmail.com>
2019-05-24 12:32:20 +0300

    Initial setup

---
+++ src/main/java/nom/side/poc/file/dbf/App.java
@@ -0,0 +1,13 @@
+package nom.side.poc.file.dbf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
